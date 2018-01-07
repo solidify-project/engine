3aa102d0758543b5fce290ae8c70a9b7938993f6
Anton Boyko <boyko.ant@live.com>
2018-01-07 17:41:38 +0200

    lazy cache implemented

---
+++ src/SolidifyProject.Engine.Utils/Cache/LazyCache.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading.Tasks;
+
+namespace SolidifyProject.Engine.Utils.Cache
+{
+    public sealed class LazyCache<T> where T : class 
+    {
+        public delegate Task<T> LoadToCacheAsyncDelegate(string key);
+
+        private readonly LoadToCacheAsyncDelegate _loadToCache;
+        
+        private readonly ConcurrentDictionary<string, Lazy<Task<T>>> _cache= new ConcurrentDictionary<string, Lazy<Task<T>>>();
+
+        public LazyCache(LoadToCacheAsyncDelegate loadToCache)
+        {
+            _loadToCache = loadToCache;
+        }
+
+        public async Task<T> GetFromCacheAsync(string key)
+        {
+            if (_cache.TryGetValue(key, out var result))
+            {
+                return await result.Value;
+            }
+
+            result = new Lazy<Task<T>>(() => _loadToCache(key));
+            
+            _cache.AddOrUpdate(key, result, (k, r) => result);
+
+            return await GetFromCacheAsync(key);
+        }
+    }
+}
