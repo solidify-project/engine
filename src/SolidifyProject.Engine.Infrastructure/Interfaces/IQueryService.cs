9e202a265a2134fdb6da87f5b94bbbd93bd2f093
Anton Boyko <boyko.ant@live.com>
2018-01-21 19:44:20 +0200

    started with query service

---
+++ src/SolidifyProject.Engine.Infrastructure/Interfaces/IQueryService.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SolidifyProject.Engine.Infrastructure.Interfaces
+{
+    public interface IQueryService
+    {
+        Task<IEnumerable<dynamic>> ExecuteQueryAsync(
+            string prefix = null,
+            int? top = null,
+            int? skip = null);
+    }
+}
