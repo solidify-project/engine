e430bc23f7ef17bda850121754412faa9dad6a65
Anton Boyko <boyko.ant@live.com>
2018-01-26 00:26:12 +0200

    started with query service

---
+++ src/SolidifyProject.Engine.Infrastructure/Services/QueryService.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace SolidifyProject.Engine.Infrastructure.Services
+{
+    public sealed class QueryService
+    {
+        private readonly dynamic _data;
+        
+        public QueryService(dynamic data)
+        {
+            _data = data;
+        }
+        
+        public IEnumerable<dynamic> Query(
+            string filterString = null,
+            string orderString = null,
+            int? top = null,
+            int? skip = null)
+        {
+            IEnumerable<dynamic> results = _data.__collection;
+            
+            if (skip.HasValue)
+            {
+                results = results.Skip(skip.Value);
+            }
+
+            if (top.HasValue)
+            {
+                results = results.Take(top.Value);
+            }
+
+            yield return results;
+        }
+
+        public int Count(string filterString = null)
+        {
+            var results = (IEnumerable<dynamic>) _data.__collection;
+
+            if (results.Any())
+            {
+                return results.Count();
+            }
+
+            return 0;
+        }
+    }
+}
