3f19b39775328a3cafba0efc1eab6249ca3088f8
L0ndra <illialubenets@yandex.ua>
2018-06-08 21:44:39 +0300

    transform tests to easy form

--- src/Test/SolidifyProject.Engine.Test.Unit/Infrastructure/Models/CustomDataModel/Parse/XmlCustomDataModelTest.cs
+++ src/Test/SolidifyProject.Engine.Test.Unit/Infrastructure/Models/CustomDataModel/Parse/XmlCustomDataModelTest.cs
@@ -6,15 +6,41 @@ using SolidifyProject.Engine.Infrastructure.Enums;
 
 namespace SolidifyProject.Engine.Test.Unit.Infrastructure.Models.CustomDataModel.Parse
 {
-   [TestFixture]
-   public class XmlCustomDataModelTest
-   {
-       private static IEnumerable<(string, Func<dynamic, int, string>, Func<dynamic, int, string>)> _xmlSources
-       {
-           get
-           {
-               yield return (
-                   @"<root>
+    [TestFixture]
+    public class XmlCustomDataModelTest
+    {
+        [Test]
+        public void ParseXmlOnlyWithAttributes()
+        {
+            var model = new Engine.Infrastructure.Models.CustomDataModel();
+            model.Id = "file.xml";
+            model.ContentRaw = @"<root>
+                    <links name=""facebook"" url=""https://facebook.com"" />
+                    <links name=""twitter"" url=""https://twitter.com"" />
+               </root>";
+
+            model.Parse();
+
+            Assert.NotNull(model.DataType);
+            Assert.AreEqual(CustomDataType.Xml.ToString(), model.DataType.ToString());
+
+            var data = (dynamic) model.CustomData;
+            Assert.IsNotNull(data);
+            Assert.AreEqual(2, data.root.links.Count);
+
+            Assert.AreEqual("facebook", data.root.links[0]["@name"]);
+            Assert.AreEqual("https://facebook.com", data.root.links[0]["@url"]);
+
+            Assert.AreEqual("twitter", data.root.links[1]["@name"]);
+            Assert.AreEqual("https://twitter.com", data.root.links[1]["@url"]);
+        }
+
+        [Test]
+        public void ParseXmlOnlyWithFields()
+        {
+            var model = new Engine.Infrastructure.Models.CustomDataModel();
+            model.Id = "file.xml";
+            model.ContentRaw = @"<root>
                    <links>
                       <name>facebook</name>
                       <url>https://facebook.com</url>
@@ -23,68 +49,79 @@ namespace SolidifyProject.Engine.Test.Unit.Infrastructure.Models.CustomDataModel
                        <name>twitter</name>
                        <url>https://twitter.com</url>
                    </links>
-               </root>",
-                   (data, indx) => data.root.links[indx].name,
-                   (data, indx) => data.root.links[indx].url
-               );
-               yield return (
-                   @"<root>
-                    <links name=""facebook"" url=""https://facebook.com"" />
-                    <links name=""twitter"" url=""https://twitter.com"" />
-               </root>",
-                    (data, indx) => data.root.links[indx]["@name"],
-                    (data, indx) => data.root.links[indx]["@url"]
-                   );
-               yield return (
-                   @"<root>
+               </root>";
+
+            model.Parse();
+
+            Assert.NotNull(model.DataType);
+            Assert.AreEqual(CustomDataType.Xml.ToString(), model.DataType.ToString());
+
+            var data = (dynamic) model.CustomData;
+            Assert.IsNotNull(data);
+            Assert.AreEqual(2, data.root.links.Count);
+
+            Assert.AreEqual("facebook", data.root.links[0].name);
+            Assert.AreEqual("https://facebook.com", data.root.links[0].url);
+
+            Assert.AreEqual("twitter", data.root.links[1].name);
+            Assert.AreEqual("https://twitter.com", data.root.links[1].url);
+        }
+
+        [Test]
+        public void ParseXmlWithFieldAndAttribute()
+        {
+            var model = new Engine.Infrastructure.Models.CustomDataModel();
+            model.Id = "file.xml";
+            model.ContentRaw = @"<root>
                    <links name=""facebook"">
                        <url>https://facebook.com</url>
                    </links>
                    <links name=""twitter"">
                        <url>https://twitter.com</url>
                    </links>
-               </root>",
-                   (data, indx) => data.root.links[indx]["@name"],
-                   (data, indx) => data.root.links[indx].url
-               );
-               yield return (
-                   @"<root>
+               </root>";
+
+            model.Parse();
+
+            Assert.NotNull(model.DataType);
+            Assert.AreEqual(CustomDataType.Xml.ToString(), model.DataType.ToString());
+
+            var data = (dynamic) model.CustomData;
+            Assert.IsNotNull(data);
+            Assert.AreEqual(2, data.root.links.Count);
+
+            Assert.AreEqual("facebook", data.root.links[0]["@name"]);
+            Assert.AreEqual("https://facebook.com", data.root.links[0].url);
+
+            Assert.AreEqual("twitter", data.root.links[1]["@name"]);
+            Assert.AreEqual("https://twitter.com", data.root.links[1].url);
+        }
+
+        [Test]
+        public void ParseXmlWithAttributeAndValue()
+        {
+            var model = new Engine.Infrastructure.Models.CustomDataModel();
+            model.Id = "file.xml";
+            model.ContentRaw = @"<root>
                    <links name=""facebook"">https://facebook.com</links>
                    <links name=""twitter"">https://twitter.com</links>
-               </root>",
-                    (data, indx) => data.root.links[indx]["@name"],
-                    (data, indx) => data.root.links[indx]["#text"]
-               );
+               </root>";
 
-           }
+            model.Parse();
 
-       }
-      
-      
-      
-      [Test]
-      [TestCaseSource(nameof(_xmlSources))]
-      public void ParseXml((string xml, Func<dynamic, int, string> nameGetter, Func<dynamic, int, string> urlGetter) tuple)
-      {
-         var model = new Engine.Infrastructure.Models.CustomDataModel();
-         model.Id = "file.xml";
-         model.ContentRaw = tuple.xml;
-            
-         model.Parse();
-            
-         Assert.NotNull(model.DataType);
-         Assert.AreEqual(CustomDataType.Xml.ToString(), model.DataType.ToString());
-            
-         var data = (dynamic)model.CustomData;
-         Assert.IsNotNull(data);
-         Assert.AreEqual(2, data.root.links.Count);
-            
-         Assert.AreEqual("facebook", tuple.nameGetter(data, 0));
-         Assert.AreEqual("https://facebook.com", tuple.urlGetter(data, 0));
-            
-         Assert.AreEqual("twitter", tuple.nameGetter(data, 1));
-         Assert.AreEqual("https://twitter.com", tuple.urlGetter(data, 1));
-      }
-      
-   }
+            Assert.NotNull(model.DataType);
+            Assert.AreEqual(CustomDataType.Xml.ToString(), model.DataType.ToString());
+
+            var data = (dynamic) model.CustomData;
+            Assert.IsNotNull(data);
+            Assert.AreEqual(2, data.root.links.Count);
+
+            Assert.AreEqual("facebook", data.root.links[0]["@name"]);
+            Assert.AreEqual("https://facebook.com", data.root.links[0]["#text"]);
+
+            Assert.AreEqual("twitter", data.root.links[1]["@name"]);
+            Assert.AreEqual("https://twitter.com", data.root.links[1]["#text"]);
+        }
+
+    }
 }
