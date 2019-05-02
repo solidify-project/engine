ea4301d2de11dec411e91809ffc5f904e5f25a66
Anton Boyko <me@boykoant.pro>
2019-05-02 22:59:03 +0300

    replaced mustache rendering library

--- src/SolidifyProject.Engine.Services/TemplateService/MustacheTemplateService.cs
+++ src/SolidifyProject.Engine.Services/TemplateService/MustacheTemplateService.cs
@@ -2,28 +2,30 @@
 using System.Dynamic;
 using System.IO;
 using System.Threading.Tasks;
-using Nustache.Core;
 using SolidifyProject.Engine.Infrastructure.Interfaces;
 using SolidifyProject.Engine.Infrastructure.Models;
 using SolidifyProject.Engine.Infrastructure.Models.Base;
-using SolidifyProject.Engine.Utils.Cache;
+using Stubble.Core.Contexts;
+using Stubble.Core.Exceptions;
+using Stubble.Core.Interfaces;
+using Stubble.Core.Renderers.StringRenderer;
+using Stubble.Core.Settings;
 
 namespace SolidifyProject.Engine.Services.TemplateService
 {
     public sealed class MustacheTemplateService : ITemplateService
     {
-        private readonly Encoders.HtmlEncoder _noHtmlEncoding = delegate(string text) { return text; };
         private readonly IContentReaderService<TextContentModel> _partialsLocator;
 
-        private readonly LazyCache<Template> _cache;
+        internal RendererSettings RendererSettings = new RendererSettingsBuilder().BuildSettings();
+
         
         public MustacheTemplateService(IContentReaderService<TextContentModel> partialsLocator = null)
         {
             _partialsLocator = partialsLocator;
-            _cache = new LazyCache<Template>(loadTemplate);
         }
         
-        public Task<string> RenderTemplateAsync(string template, PageModel pageModel, ExpandoObject dataModel)
+        public async Task<string> RenderTemplateAsync(string template, PageModel pageModel, ExpandoObject dataModel)
         {
             if (pageModel == null)
             {
@@ -32,35 +34,35 @@ namespace SolidifyProject.Engine.Services.TemplateService
 
             var model = new { Page = pageModel, Data = dataModel };
             
-            var result = Render.StringToString(template, model, getTemplate, new RenderContextBehaviour
+            var loader = new StubbleLoader(_partialsLocator);
+            
+            return await RenderAsync(template, model, loader, new RenderSettings
             {
-                HtmlEncoder = _noHtmlEncoding
+                SkipHtmlEncoding = true,
+                ThrowOnDataMiss = true
             });
-
-            return Task.FromResult(result);
         }
 
-        private Template loadTemplate(string name)
+        private async Task<string> RenderAsync(string template, object view, IStubbleLoader partialsLoader, RenderSettings settings)
         {
-            if (_partialsLocator == null)
+            var loadedTemplate = await RendererSettings.TemplateLoader.LoadAsync(template);
+
+            if (loadedTemplate == null)
             {
-                return null;
+                throw new UnknownTemplateException("No template was found with the name '" + template + "'");
             }
 
-            var content = _partialsLocator.LoadContentByIdAsync(name).Result;
+            var document = RendererSettings.Parser.Parse(loadedTemplate, RendererSettings.DefaultTags, pipeline: RendererSettings.ParserPipeline);
 
-            using (var stream = new StringReader(content.ContentRaw))
+            using (var textwriter = new StringWriter())
             {
-                var template = new Template();
-                template.Load(stream);
-                
-                return template;
-            }
-        }
+                var renderer = new StringRender(textwriter, RendererSettings.RendererPipeline, RendererSettings.MaxRecursionDepth);
 
-        private Template getTemplate(string key)
-        {
-            return _cache.GetFromCache(key);
+                await renderer.RenderAsync(document, new Context(view, RendererSettings, partialsLoader, settings ?? RendererSettings.RenderSettings));
+
+                renderer.Writer.Flush();
+                return ((StringWriter)renderer.Writer).ToString();
+            }
         }
     }
 }
